95156c8b0a104d6df65d0116a89aa039896914d0
sagar7378 <sagarprajapat250@gmail.com>
2025-07-08 21:33:11 +0530

    office work code push

---
+++ config.js
@@ -0,0 +1,16 @@
+import 'dotenv/config';
+import mongoose from 'mongoose';
+
+const mongoURI = process.env.MONGO_URI;
+
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected successfully'))
+.catch(err => console.error('MongoDB connection error:', err));
+
+const config = mongoose;
+
+export default config;
+
--- index.js
+++ index.js
@@ -1,70 +1,17 @@
-import config from "./config.js";
 import dotenv from "dotenv";
+import connectDB from "./src/config/db.js";
+import { server } from "./src/app.js";
+
 dotenv.config();
-import express from "express";
-import http from "http";
-import router from "./routes.js";
-import { Server } from "socket.io";
 
-const app = express();
-const server = http.createServer(app);
-const activeSockets = {};  
-const users = {}; // Store connected users and their socket IDs
 
-const io = new Server(server, {
-        cors: { origin: "*",   
-        methods: ["GET", "POST"]
-    },
+connectDB(process.env.MONGO_URL)
+  .then(() => {
+    server.listen(process.env.PORT, () => {
+      console.log(`🚀 Server ready at http://localhost:${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("❌ DB connection failed:", err);
   });
-
-  // ✅ Socket Connection
-io.on("connection", (socket) => {
-    console.log(`User Connected: ${socket.id}`);
-
-    socket.onAny((event, data) => {
-        console.log(`📩 Received event: ${event}`, data);
-    });
-  
-    socket.on("register", (userId) => {
-        console.log("🛑 Received register event with userId:", userId); // Debugging log
-
-        if (!userId) {
-            console.log("⚠️ No userId received!");
-            return;
-        }
-
-        users[userId] = socket.id;
-        console.log(`✅ User ${userId} registered with socket ${socket.id}`);
-
-        // Send confirmation back
-        socket.emit("registered", { message: "Registered successfully" });
-    });
-
-    socket.on("disconnect", () => {
-        console.log("User disconnected:", socket.id);
-        for (const [userId, socketId] of Object.entries(users)) {
-            if (socketId === socket.id) {
-                delete users[userId];
-                break;
-            }
-        }
-    });
-  });
-
-
-app.use(express.json());
-app.use("/api", router);
-
-// Function to emit messages only to a specific user
-export function sendMessageToUser(receiverId, messageData) {
-    const receiverSocketId = activeSockets[receiverId];
-    console.log(`Sending message to ${receiverId}:`, messageData);
-    if (receiverSocketId) {
-        io.to(receiverSocketId).emit("newMessage", messageData);
-    }
-}
-
-server.listen(process.env.PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${process.env.PORT}`);
-});
-
+ 
